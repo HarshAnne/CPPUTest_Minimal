843febe719530b418e6b30c8e40dece94f124fcb
Harsh <harshanne1996@gmail.com>
2021-01-18 00:30:45 -0500

    Simple test group and two tests, 1 passing and one failing

---
+++ Test/dummy_test.cpp
@@ -0,0 +1,18 @@
+#include "CppUTest/TestHarness.h"
+
+// create a test group
+TEST_GROUP(dummy_test)
+{
+};
+
+// create a test for that test group
+TEST(dummy_test, pass_me)
+{
+    CHECK_EQUAL(1,1);
+}
+
+// create test to confirm fail works
+TEST(dummy_test, fail_me)
+{
+    CHECK_EQUAL(1,0);
+}
